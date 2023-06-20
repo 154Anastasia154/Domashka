1abd9a8dd703a8c89f427e9599dd134a0bed0d9b
Anastasia <slavjanka54@mail.ru>
2023-06-20 18:46:59 +0700

    zadah23

---
+++ zadaha_23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов чисел от 1 до {0}", n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine("{0}^3 = {1}", i, Math.Pow(i, 3));
+        }
+
+        Console.ReadKey();
+    }
+}
