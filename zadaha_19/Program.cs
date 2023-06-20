ac0c13f1b795d1fc087566a4be0b9344d10c5f96
Anastasia <slavjanka54@mail.ru>
2023-06-20 18:50:56 +0700

    zadaha19

---
+++ zadaha_19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int firstDigit = number / 10000;
+        int secondDigit = (number / 1000) % 10;
+        int fourthDigit = (number / 10) % 10;
+        int fifthDigit = number % 10;
+
+        bool isPalindrome = (firstDigit == fifthDigit) && (secondDigit == fourthDigit);
+
+        if (isPalindrome) {
+            Console.WriteLine($"{number} - палиндром");
+        } else {
+            Console.WriteLine($"{number} - не палиндром");
+        }
+    }
+}
