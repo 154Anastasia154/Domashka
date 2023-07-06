f6985243e3a0420ce99c3d2f820243fafe4b44be
Anastasia <slavjanka54@mail.ru>
2023-07-07 00:07:54 +0700

    seminar7

---
+++ Seminar7_C#/Program.cs
@@ -0,0 +1,122 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 3;
+        int n = 4;
+
+        double[,] array = new double[m, n];
+        Random random = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = random.NextDouble() * 20 - 10; // Генерация случайного числа в диапазоне [-10, 10)
+            }
+        }
+
+        // Вывод массива на консоль
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        int targetRow = 2;
+        int targetColumn = 3;
+
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        if (targetRow >= 0 && targetRow < rows && targetColumn >= 0 && targetColumn < columns)
+        {
+            int element = array[targetRow, targetColumn];
+            Console.WriteLine("Значение элемента: " + element);
+        }
+        else
+        {
+            Console.WriteLine("Такого элемента нет.");
+        }
+    }
+}
+
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        double[] columnAverages = new double[columns];
+
+        for (int j = 0; j < columns; j++)
+        {
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, j];
+            }
+            columnAverages[j] = (double)sum / rows;
+        }
+
+        Console.WriteLine("Среднее арифметическое каждого столбца:");
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(columnAverages[j].ToString("F1") + " ");
+        }
+    }
+}
